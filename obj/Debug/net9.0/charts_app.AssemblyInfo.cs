4f21f38ff6d04c2469c4f5eeed0f074c6fb01c13
mikklu04 <miki.kluba@gmail.com>
2025-03-27 14:13:53 +0100

    Added models folder

--- obj/Debug/net9.0/charts_app.AssemblyInfo.cs
+++ obj/Debug/net9.0/charts_app.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("charts_app")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b8b037e9b878dbc5b4012bfb8340349abafd069e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cf85f5c8c57d4fe0b591739d9cacf119a9d0f123")]
 [assembly: System.Reflection.AssemblyProductAttribute("charts_app")]
 [assembly: System.Reflection.AssemblyTitleAttribute("charts_app")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
